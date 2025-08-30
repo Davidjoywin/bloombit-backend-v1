fe41c69fe1046f6f1862c6bf3fa31231dd4736c5
David Joy <davidjoy.win@gmail.com>
2025-08-30 12:54:59 +0100

    update the auth endpoint error handler

--- account/views/auth.py
+++ account/views/auth.py
@@ -23,30 +23,40 @@ class Auth(APIView):
         serializer = self.serializer_class(data=request.data, context={'request': request})
 
         if serializer.is_valid():
-            user = UserProfile.objects.get(username=serializer.validated_data['username'])
-            if not user.check_password(serializer.validated_data.get('password', '')):
+            try:
+                user = UserProfile.objects.get(username=serializer.validated_data['username'])
+                if not user.check_password(serializer.validated_data.get('password', '')):
+                    return Response(
+                        {
+                            'status': False,
+                            'message': "Login failed",
+                            'error': {'password': ['Incorrect password']},
+                            'statusCode': status.HTTP_400_BAD_REQUEST
+                        },
+                        status=status.HTTP_400_BAD_REQUEST
+                    )
+                login(request, user)
+                user_serializer = UserProfileSerializer(user, context={'request': request})
+                token = create_token(user)
+                return Response(
+                    {
+                        'status': True,
+                        'message': "Login successfully",
+                        'user': user_serializer.data,
+                        'token': token,
+                        'statusCode': status.HTTP_200_OK
+                    },
+                    status=status.HTTP_200_OK
+                )
+            except:
                 return Response(
                     {
                         'status': False,
-                        'message': "Login failed",
-                        'errors': {'password': ['Incorrect password']},
+                        'message': "Login Failed",
+                        'error': {'username': ['User not found']},
                         'statusCode': status.HTTP_400_BAD_REQUEST
-                    },
-                    status=status.HTTP_400_BAD_REQUEST
+                    }
                 )
-            login(request, user)
-            user_serializer = UserProfileSerializer(user, context={'request': request})
-            token = create_token(user)
-            return Response(
-                {
-                    'status': True,
-                    'message': "Login successfully",
-                    'user': user_serializer.data,
-                    'token': token,
-                    'statusCode': status.HTTP_200_OK
-                },
-                status=status.HTTP_200_OK
-            )
         return Response(
             {
                 'status': False,
